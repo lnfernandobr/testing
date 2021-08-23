c9171a8d09e5d861af0634b5c80ec8bddfe08f0e
Fernando <lnfernandobr@gmail.com>
2021-08-23 10:46:20 -0400

    - Retry and wait command

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -19,16 +19,29 @@ describe("Syncs", () => {
     cy.get("#novoCampo").should("exist");
   });
 
-  it.only("Find command", () => {
+  it("Find command", () => {
     cy.get("#buttonList").click();
 
     cy.get("#lista li").find("span").should("contain", "Item 1");
 
-    // Da erro, porque a lista eh assincrona, ou seja, demos um get numa lista li aonde tinha somente
+    // Da erro, porque a lista eh assíncrona, ou seja, demos um get numa lista li aonde tinha somente
     // um item no span, e quando o find foi executado, ou item 2 foi criado. Gerando o erro, o retries do
     // cypress nao funcionou porque ele tenta somente os comandos abaixo do que gerou o erro, e o comando
     // pai do que gerou o erro, ou seja, quem gerou o erro foi o should, nao existia o item 1, entao, ele
     // O comando find era executado, e qualquer um abaixo do sloud tambem seria.
     // cy.get("#lista li").find("span").should("contain", "Item 2");
   });
+
+  // To change default timeout settings of cypress, we need add to cypress.json an props called  defaultCommandTimeout as true
+  it.only("TimeOut", () => {
+    cy.get("#buttonDelay").click();
+    // default cypress Timeout is 4 seconds.
+    // cy.get("#novoCampo", { timeout: 10000 }).type("funciona");
+
+    // This command gonna wait for 5 seconds
+    cy.wait(5000);
+  });
+
+
+
 });
