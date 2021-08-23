c214792d7736aa8772af9e50eef54cfbe3f69e18
Fernando <lnfernandobr@gmail.com>
2021-08-23 18:05:18 -0400

    - Fixture

---
+++ cypress/integration/fixture.spec.js
@@ -0,0 +1,21 @@
+describe("Fixtures testes", () => {
+  // Called one  before all tests
+  before(() => {
+    const link = "https://wcaquino.me/cypress/componentes.html";
+    cy.visit(link);
+  });
+
+  beforeEach(() => {
+    const link = "https://wcaquino.me/cypress/componentes.html";
+    cy.visit(link);
+  });
+
+  it("Get data from fixture file ", function () {
+    cy.fixture("userData")
+      .as("user")
+      .then(function () {
+        cy.get("#formNome").type(this.user.name);
+        cy.get("#formSobrenome").type(this.user.lastName);
+      });
+  });
+});
---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,15 @@
+describe("Work with basic elements", () => {
+  // Called one  before all tests
+  before(() => {
+    const link = "https://wcaquino.me/cypress/componentes.html";
+    cy.visit(link);
+  });
+
+  beforeEach(() => {
+    const link = "https://wcaquino.me/cypress/componentes.html";
+    cy.visit(link);
+  });
+
+
+
+});
